4fcb7a4e42f24e531503bb4e548956b3b2e50d75
Alex Kim <alexkimwrites@gmail.com>
2017-02-07 14:54:02 -0800

    First commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+var movieLink = document.querySelector('#movie-link')
+var gameLink = document.querySelector('#game-link')
+var play360 = document.querySelector('#play360')
+
+
+movieLink.addEventListener('click', function () => {
+
+  
+
+})
